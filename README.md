bc4509934e2f24cc38097efb6139f7e0fc6bac7e
Steven Wood <steven.wood@uk.ibm.com>
2015-04-24 17:30:06 +0100

    Getting started...

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # sudoku-solver
-A sudoku solver in various languages
+A sudoku solver in various languages, the actual algorithm to solve the puzzles doesn't seem very efficient, but it works.  Nothing much to see here, just me attempting to learn some new languages (and implement the same solution in some I already know...)
--- js/solver.js
+++ js/solver.js
@@ -16,27 +16,27 @@
 
         solve: function(collector) {
             /*
-		        The algorithm is :
+                The algorithm is :
 
-		        1. For each cell, calculate the set of possible values by eliminating any solved
-		           values that are already in the cells peers (cells in the same row, column
-		           and sub grid)
-		        2. If after step 1 some cells only have one possible value, we can consider those
-		           cells to be solved.
-		        3. Repeat steps 1 and 2 until no more cells can be solved.
-		        4. In each row, column and sub grid, if there is only one place a value can go,
-		           then put it there and repeat steps 1 and 2 for all that cells peers.
-		        5. If the grid is not solved, start searching for a solution.  Gather all the
-		           unsolved cells and order them by the number of possible values they have, start
-		           with the lowest.
-		        6. Go through the possible values for each cell and attempt to solve the grid using
-		           all these rules, when we find an cell that does not have a value and has no possible
-		           values, or if we try every possible value for a cell and can't find a solution, we
-		           need to backtrack and then try another value.
+                1. For each cell, calculate the set of possible values by eliminating any solved
+                   values that are already in the cells peers (cells in the same row, column
+                   and sub grid)
+                2. If after step 1 some cells only have one possible value, we can consider those
+                   cells to be solved.
+                3. Repeat steps 1 and 2 until no more cells can be solved.
+                4. In each row, column and sub grid, if there is only one place a value can go,
+                   then put it there and repeat steps 1 and 2 for all that cells peers.
+                5. If the grid is not solved, start searching for a solution.  Gather all the
+                   unsolved cells and order them by the number of possible values they have, start
+                   with the lowest.
+                6. Go through the possible values for each cell and attempt to solve the grid using
+                   all these rules, when we find an cell that does not have a value and has no possible
+                   values, or if we try every possible value for a cell and can't find a solution, we
+               need to backtrack and then try another value.
 
-		        Because this method is called recursively
-		        we collect all the cells we solve in the list supplied.
-		    */
+                Because this method is called recursively
+                we collect all the cells we solve in the list supplied.
+            */
             var numSolved = 1,
                 unsolved = this.grid.unsolved();
 
@@ -68,9 +68,9 @@
 
         _exclude: function(cell, collector) {
             /*
-	            This is the process of looking at a cells subgrid, row and column and excluding from the
-	            list 1..9 all numbers that are already in the subgrid, row and column (peers).
-	        */
+               This is the process of looking at a cells subgrid, row and column and excluding from the
+                list 1..9 all numbers that are already in the subgrid, row and column (peers).
+            */
             if (cell.value !== 0) {
                 return [];
             }
@@ -94,10 +94,10 @@
 
         _findUniqueValuesInUnits: function(collector) {
             /*
-	            Looks for unique values amongst the set of possible values for a unit.
-	            If we find one, assign that value for the cell and then call _exclude
-	            on the cells peers.
-	        */
+                Looks for unique values amongst the set of possible values for a unit.
+                If we find one, assign that value for the cell and then call _exclude
+                on the cells peers.
+            */
             // get the sub grids
             var subGrids = [];
             this.grid.subgrids().forEach(function(sg) {
@@ -135,9 +135,9 @@
 
         _search: function(collector) {
             /*
-	            This is the backtracking search.  The "collector" is used to implement the
-	            backtracking, so that we can undo values that were set following a guess that leads to
-	            a dead end.
+                This is the backtracking search.  The "collector" is used to implement the
+                backtracking, so that we can undo values that were set following a guess that leads to
+                a dead end.
 	        */
 
             var cell = (this.grid.unsolved()
