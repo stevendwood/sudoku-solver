9edbe87534fddb172cc053a3e4a77bfd4fbddaea
Steven Wood <sdwood.76@gmail.com>
2020-12-28 11:53:58 +0000

    Formatting and use template literals

--- js/grid.mjs
+++ js/grid.mjs
@@ -1,83 +1,83 @@
 import Cell from "./cell.mjs";
 
 export default class Grid {
-  constructor(input) {
-    let currentRow;
-    this.rows = [];
+    constructor(input) {
+        let currentRow;
+        this.rows = [];
 
-    for (let idx = 0; idx < input.length; idx++) {
-      if (idx % 9 === 0) {
-        currentRow = [];
-        this.rows.push(currentRow);
-      }
+        for (let idx = 0; idx < input.length; idx++) {
+            if (idx % 9 === 0) {
+                currentRow = [];
+                this.rows.push(currentRow);
+            }
 
-      currentRow.push(
-        new Cell(this.rows.length - 1, currentRow.length, input[idx])
-      );
+            currentRow.push(
+                new Cell(this.rows.length - 1, currentRow.length, input[idx])
+            );
+        }
     }
-  }
 
-  toString() {
-    let output = "";
-    for (let i = 0; i < this.rows.length; i++) {
-      if (i !== 0 && i % 3 === 0) {
-        output += "---------+---------+---------\n";
-      }
+    toString() {
+        let output = "";
+        for (let i = 0; i < this.rows.length; i++) {
+            if (i !== 0 && i % 3 === 0) {
+                output += "---------+---------+---------\n";
+            }
 
-      let currentRow = this.rows[i];
-      for (let j = 0; j < currentRow.length; j++) {
-        if (j !== 0 && j % 3 === 0) {
-          output += "|";
+            let currentRow = this.rows[i];
+            for (let j = 0; j < currentRow.length; j++) {
+                if (j !== 0 && j % 3 === 0) {
+                    output += "|";
+                }
+
+                output += " " + currentRow[j].toString() + " ";
+            }
+
+            output += "\n";
         }
 
-        output += " " + currentRow[j].toString() + " ";
-      }
-
-      output += "\n";
+        return output;
     }
 
-    return output;
-  }
-
-  subgrids() {
-    if (!this.grids) {
-      this.grids = [];
-      for (let i = 0; i < 9; i += 3) {
-        for (let j = 0; j < 9; j += 3) {
-          this.grids.push(this.sameSubGridAs(new Cell(i, j)));
+    subgrids() {
+        if (!this.grids) {
+            this.grids = [];
+            for (let i = 0; i < 9; i += 3) {
+                for (let j = 0; j < 9; j += 3) {
+                    this.grids.push(this.sameSubGridAs(new Cell(i, j)));
+                }
+            }
         }
-      }
+
+        return this.grids;
     }
 
-    return this.grids;
-  }
+    columns() {
+        if (!this._columns) {
+            this._columns = [];
+            for (let i = 0; i < 9; i++) {
+                this._columns.push([]);
+            }
+            this.rows.forEach(function (row) {
+                row.forEach(function (cell, idx) {
+                    this._columns[idx].push(cell);
+                }, this);
+            }, this);
+        }
 
-  columns() {
-    if (!this._columns) {
-      this._columns = [];
-      for (let i = 0; i < 9; i++) {
-        this._columns.push([]);
-      }
-      this.rows.forEach(function (row) {
-        row.forEach(function (cell, idx) {
-          this._columns[idx].push(cell);
-        }, this);
-      }, this);
+        return this._columns;
     }
 
-    return this._columns;
-  }
+    sameRowAs(cell) {
+        return this.rows[cell.row];
+    }
 
-  sameRowAs(cell) {
-    return this.rows[cell.row];
-  }
+    sameColAs(cell) {
+        return this.columns()[cell.col];
+    }
 
-  sameColAs(cell) {
-    return this.columns()[cell.col];
-  }
-
-  sameSubGridAs(cell) {
-    /*
+    sameSubGridAs(cell) {
+        /*
             Get all the cells in the same "sub grid" as the given cell. e.g.
             for the cell "c" below the cells in the "same_sub_grid" (which are
             marked x below) are returned along with the argument cell.
@@ -95,52 +95,52 @@ export default class Grid {
             . . . | . . . | . . .
         */
 
-    // row:
-    // 0 - 2 -> 0
-    // 3 - 5 -> 3
-    // 6 - 8 -> 5
+        // row:
+        // 0 - 2 -> 0
+        // 3 - 5 -> 3
+        // 6 - 8 -> 5
 
-    // col:
-    // same as above
-    if (!cell.subgrid) {
-      let index = function (x) {
-        if (x <= 2) {
-          return 0;
-        } else if (x <= 5) {
-          return 3;
-        } else {
-          return 6;
+        // col:
+        // same as above
+        if (!cell.subgrid) {
+            let index = function (x) {
+                if (x <= 2) {
+                    return 0;
+                } else if (x <= 5) {
+                    return 3;
+                } else {
+                    return 6;
+                }
+            };
+
+            let startRow = index(cell.row),
+                startCol = index(cell.col),
+                subgrid = [];
+            for (let i = startRow; i < startRow + 3; i++) {
+                let row = this.rows[i],
+                    subGridRow = [];
+                for (let j = startCol; j < startCol + 3; j++) {
+                    subGridRow.push(row[j]);
+                }
+
+                subgrid.push(subGridRow);
+            }
+            cell.subgrid = subgrid;
         }
-      };
 
-      let startRow = index(cell.row),
-        startCol = index(cell.col),
-        subgrid = [];
-      for (let i = startRow; i < startRow + 3; i++) {
-        let row = this.rows[i],
-          subGridRow = [];
-        for (let j = startCol; j < startCol + 3; j++) {
-          subGridRow.push(row[j]);
-        }
-
-        subgrid.push(subGridRow);
-      }
-      cell.subgrid = subgrid;
+        return cell.subgrid;
     }
 
-    return cell.subgrid;
-  }
+    unsolved() {
+        return this.rows.flat().filter((c) => c.value === 0);
+    }
 
-  unsolved() {
-    return this.rows.flat().filter((c) => c.value === 0);
-  }
+    isSolved() {
+        return !this.rows.flat().some((x) => x.value === 0);
+    }
 
-  isSolved() {
-    return !this.rows.flat().some((x) => x.value === 0);
-  }
-
-  peers(cell) {
-    /*
+    peers(cell) {
+        /*
             Get the peers for the cell.  The peers for the cell "c" are pictorially
             represented below by the cells marked "x"
 
@@ -156,17 +156,17 @@ export default class Grid {
             . x . | . . . | . . .
             . x . | . . . | . . .
         */
-    if (!cell.peers) {
-      cell.peers = Array.from(
-        new Set(
-          this.sameColAs(cell)
-            .concat(this.sameRowAs(cell))
-            .concat(this.sameSubGridAs(cell).flat())
-            .filter((x) => x !== cell)
-        )
-      );
-    }
+        if (!cell.peers) {
+            cell.peers = Array.from(
+                new Set(
+                    this.sameColAs(cell)
+                        .concat(this.sameRowAs(cell))
+                        .concat(this.sameSubGridAs(cell).flat())
+                        .filter((x) => x !== cell)
+                )
+            );
+        }
 
-    return cell.peers;
-  }
+        return cell.peers;
+    }
 }
--- js/main.mjs
+++ js/main.mjs
@@ -3,50 +3,46 @@ import Grid from "./grid.mjs";
 import Solver from "./solver.mjs";
 
 (async () => {
-  try {
-    const data = await fs.readFile("puzzles.txt", "utf-8");
-    let puzzles = data.split("\n"),
-      totalTime = 0,
-      puzzleCount = 0,
-      totalGuesses = 0;
+    try {
+        const data = await fs.readFile("puzzles.txt", "utf-8");
+        let puzzles = data.split("\n"),
+            totalTime = 0,
+            puzzleCount = 0,
+            totalGuesses = 0;
 
-    puzzles.forEach((puzzle) => {
-      puzzleCount++;
+        puzzles.forEach((puzzle) => {
+            puzzleCount++;
 
-      let g = new Grid(puzzle.trim()),
-        solver = new Solver(g),
-        start;
+            let g = new Grid(puzzle.trim()),
+                solver = new Solver(g),
+                start;
 
-      console.log(g.toString());
+            console.log(g.toString());
 
-      start = Date.now();
-      solver.solve();
-      let time = Date.now() - start;
-      totalTime += time;
-      totalGuesses += solver.guesses;
-      if (g.isSolved()) {
-        console.log(g.toString());
+            start = Date.now();
+            solver.solve();
+            let time = Date.now() - start;
+            totalTime += time;
+            totalGuesses += solver.guesses;
+            if (g.isSolved()) {
+                console.log(g.toString());
+                console.log(
+                    `solved in ${time} ms with ${solver.guesses} guesses.`
+                );
+            } else {
+                console.log("Couldn't solve it.");
+                console.log(g.toString());
+            }
+            console.log("\n");
+        });
+
         console.log(
-          "solved in " + time + "ms with " + solver.guesses + " guesses."
+            `Solved ${puzzleCount} in avg ${Math.round(
+                totalTime / puzzleCount
+            )} ms. Total time ${totalTime} ms`
         );
-      } else {
-        console.log("Couldn't solve it.");
-        console.log(g.toString());
-      }
-      console.log("\n");
-    });
-
-    console.log(
-      "Solved " +
-        puzzleCount +
-        " in avg " +
-        Math.round(totalTime / puzzleCount) +
-        " ms. Total time :" +
-        totalTime +
-        "ms"
-    );
-    console.log("Had to make " + totalGuesses + " guesses");
-  } catch (err) {
-    console.error("Failed to load puzzles...", err);
-  }
+        console.log(`Had to make ${totalGuesses} guesses`);
+    } catch (err) {
+        console.error("Failed to load puzzles...", err);
+    }
 })();
